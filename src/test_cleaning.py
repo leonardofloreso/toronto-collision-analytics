e5786c5a1dcc9abd38075494ab38ed535a4a2f59
leonardofloreso <leonardofloresolaya@gmail.com>
2026-03-16 12:55:10 -0400

    test: expand analytics coverage and clean misplaced src tests

--- src/test_cleaning.py
+++
@@ -1,7 +0,0 @@
-from cleaning import (
-    clean_missing_neighbourhood_values,
-    remove_invalid_geographic_coordinates
-)
-
-df = clean_missing_neighbourhood_values(df, column="NEIGHBOURHOOD_158")
-df = remove_invalid_geographic_coordinates(df, lat_col="LATITUDE", lon_col="LONGITUDE")
